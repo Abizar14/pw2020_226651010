8333406e3991ceee6efa3840a11b72f311ccad1a
abizar14 <abizarzauli24@gmail.com>
2023-03-11 09:32:37 +0800

    telah menambahkan database di pertemuan 10

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Mar 2023 pada 01.58
+-- Waktu pembuatan: 11 Mar 2023 pada 02.31
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ti_226651010`
+-- Database: `tim_226651010`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mahasiswa` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `nim` varchar(9) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `jurusan` varchar(128) NOT NULL,
+  `gambar` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
